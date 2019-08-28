d5f57827a20c21877fcbd1d0419fd998ed13ce54
lzy <mlzy7318@163.com>
2019-08-28 19:07:39 +0800

    lzy 小程序之swiper实现tab切换

--- myWechatDemo/pages/views/products/ylSmart1908004/index.js
+++ myWechatDemo/pages/views/products/ylSmart1908004/index.js
@@ -4,9 +4,24 @@ Page({
      * 页面的初始数据
      */
     data: {
-      isClick:true,
-      interval: '2000',// 自动切换时间间隔
-      duration: '300',// 滑动动画时长
+      tabId:'',
+      tabs:[
+        {
+          id:"1",
+          content:"标签1"
+        },
+        {
+          id:"2",
+          content:"标签2"
+        },
+        {
+          id:"3",
+          content:"标签3"
+        }
+      ],
+      duration: 300,// 滑动动画时长
+      current: 0,// 当前所在滑块的 index
+      currentItemId: 1,// 设置默认一开始显示第几个滑块
       imgUrls: [
         'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
         'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
@@ -19,25 +34,34 @@ Page({
     */ 
     tabClick(e){
       // console.log('----------this', this);
-      console.log('----------------e', e.target.id);
-      let id = e.target.id;
-      // let ele = document.getElementsById('tab1');
-      // console.log('========ele', ele);
-      let query = wx.createSelectorQuery().select('tab1')
-      console.log('========query', query);
+      // console.log('----------wx', wx);
+      // console.log('----------e', e);
+      // 控制tab标签的切换
       this.setData({
-        "background":"red"
-      })
-      
-      
-      
+        tabId:e.target.id,
+        current:e.target.id - 1
+      });
+    },
+    // 滑动切换
+    swiperChange(e){
+      // console.log('----------eSW', e);
+      if(e.detail.source === 'touch') {
+        this.setData({
+          tabId:e.detail.current + 1 + '',
+          current:e.detail.current
+        })
+      }
     },
     
     /**
      * 生命周期函数--监听页面加载`
      */
     onLoad: function (options) {
-  
+      // 初始化数据
+      this.setData({
+        tabId:'1',
+        current:0
+      });
     },
   
     /**
