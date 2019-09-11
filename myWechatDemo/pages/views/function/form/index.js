23e6f7cb049fa578bcac3ed77dde56f1abb2b9a8
lzy <mlzy7318@163.com>
2019-09-11 15:42:38 +0800

    lzy 优化代码

--- myWechatDemo/pages/views/function/form/index.js
+++ myWechatDemo/pages/views/function/form/index.js
@@ -4,73 +4,37 @@ Page({
      * 页面的初始数据
      */
     data: {
-  
+      phone : ''
     },
-    
     /**
-    * 自定义函数
-    */ 
-    formSubmit: function(e) {
-      console.log('--------------form发生了submit事件，携带数据为：', e.detail.value)
-    },
-    formReset: function() {
-      console.log('-----------------form发生了reset事件')
-    },
-
-  
-    /**
-     * 生命周期函数--监听页面加载
-     */
+    * 生命周期函数--监听页面加载
+    */
     onLoad: function (options) {
   
     },
-  
     /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-  
+    * 自定义函数
+    */ 
+    formSubmit: function(e) {// 提交表单
+      console.log('--------------form发生了submit事件，携带数据为：', e.detail.value)
+      // 掉接口传值
+      // ....
     },
-  
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-  
+    formReset: function() {// 重置表单
+      console.log('-----------------form发生了reset事件')
     },
-  
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-  
+
+
+    phoneBlur(){
+      wx.showToast({
+        title : "请输入11位手机号码",
+        icon : "none"
+      })
     },
-  
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-  
-    },
-  
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-  
-    },
-  
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-  
-    },
-  
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-  
+    phoneInput(){
+      wx.showToast({
+        title : "请输入11位手机号码",
+        icon : "none"
+      })
     }
   })
