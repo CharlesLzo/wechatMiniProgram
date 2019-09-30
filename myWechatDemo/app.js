02e8409b50801bd02cdd02bf4183b402e5847151
lzy <mlzy7318@163.com>
2019-09-30 19:36:58 +0800

    lzy 继续探索watch监听器原理

--- myWechatDemo/app.js
+++ myWechatDemo/app.js
@@ -3,53 +3,48 @@ App({
   onLaunch: function () {
 
   },
-  setWatcher:function(){
-    Object.defineProperty(this,'globalData',{
-      get:()=>{
-        return this._globalData
+  setWatcher: function(key, fn){
+    console.log('------要监听的数据', key);
+
+    var obj = this.globalData;
+    console.log('---------globalData', obj);
+    
+    var ele = obj[key];//obj[key]这个不能放在Object.defineProperty里
+    if(ele){
+      fn(ele);
+    }
+    console.log('-----------------ele', ele);
+    
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      set: function (value) {
+        console.log('--------->this:', this);
+
+        console.log('--------set时的key值', key);
+
+        this['_' + key] = value;
+        fn(value);    //数据有变化的时候回调函数，实现同步功能
       },
-      set:(value)=>{
-        let current = getCurrentPages().reverse()[0]
-        if(current.data.globalData){
-          current.setData({
-            globalData: value
-          })
+      get: function () {
+        // 可以在这里打印一些东西，然后在其他界面调用getApp().globalData.name的时候，这里就会执行。
+        console.log('--------get时的key值', key);
+
+        if (typeof this['__' + key] == 'undefined'){
+          if(ele){
+              //这里读取数据的时候隐藏变量和 globalData设置不一样，所以要做同步处理
+              this['__' + key] = ele;
+              return ele;
+          }else{
+              return undefined;
+          }
+        }else{
+            return this['__' + key];
         }
-        this._globalData = value
       }
     })
   },
-  // setWatcher: function(key, fn){
-  //   console.log('------要监听的数据', key);
-
-  //   var obj = this.globalData;
-  //   console.log('---------globalData', obj);
-    
-  //   // var ele = obj[key];//obj[key]这个不能放在Object.defineProperty里
-  //   // if(ele){
-  //   //   fn(ele);
-  //   // }
-  //   Object.defineProperty(obj, key, {
-  //     configurable: true,
-  //     enumerable: true,
-  //     set: function (value) {
-  //       console.log('--------->this:', this);
-        
-  //       console.log('--------set时的key值', key);
-
-  //       this['_' + key] = value;
-  //       fn(value);    //数据有变化的时候回调函数，实现同步功能
-  //     },
-  //     get: function () {
-  //       // 可以在这里打印一些东西，然后在其他界面调用getApp().globalData.name的时候，这里就会执行。
-  //       console.log('--------get时的key值', key);
-
-  //       return this._name;
-  //     }
-  //   })
-  // },
-  // globalData: {
-  //   userInfo: null,
-  // },
-  _globalData: {},
+  globalData: {
+    userInfo: null,
+  },
 })
--- myWechatDemo/pages/views/Monitor/index.js
+++ myWechatDemo/pages/views/Monitor/index.js
@@ -1,24 +1,98 @@
 var App = getApp();
 
+function watcher(key) {
+  let changeValue = null;
+  let resArr = [];
+  let pattern = {
+    get: function() {
+        console.log('get!');
+
+      return changeValue;
+    },
+    set: function(value) {
+      changeValue = value;
+      resArr.push({ key: changeValue });
+    }
+  };
+
+  Object.defineProperty(this, key, pattern);
+
+  this.getWatching = function(){ 
+    return resArr; 
+  };
+}
+
+function Archiver() {
+  var temperature = null;
+  var archive = [];
+
+  Object.defineProperty(this, 'temperature', {
+    get: function() {
+      console.log('get!');
+      return temperature;
+    },
+    set: function(value) {
+      temperature = value;
+      archive.push({ val: temperature });
+    }
+  });
+
+  this.getArchive = function() { return archive; };
+}
+
+
+
+
+
+
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    name: ''
+    idea: '',
+    newIdea: ''
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var _this = this;
-    App.setWatcher('name', _this.watch())
-  },
-  /**
-  * 自定义函数
-  */ 
-  watch(value){
-    console.log('---------回调函数返回的值', value)
+  onLoad: function (options) {    
     
+  },
+  checkValue(e){
+    let ele = this.data.idea;// oldValue
+    let val = e.detail.value;// newValue
+    console.log('--------oldValue', ele);
+    console.log('--------newValue', val);
+    
+    if(val === ele) return;
+    if(val != ele){
+      console.log('爸爸触发了');
+      
+      let obj = new watcher('newObj');
+      obj.changeValue;
+      obj.changeValue = val;
+      let changeValue = obj.getWatching();
+      console.log('-=-=-=-=-=-===changeValue', changeValue);
+      this.setData({newIdea: changeValue});
+      console.log('--------this.data.newIdea', this.data.newIdea);
+      
+    }
+  },
+  getValue(){
+    var arc = new Archiver();
+    arc.temperature; // 'get!'
+    arc.temperature = 11;
+    arc.temperature = 13;
+    var res = arc.getArchive(); // [{ val: 11 }, { val: 13 }]
+    console.log('-=-=-=-=-=-=--=-res', res);
+  },
+  otherEvent(){
+    // let Person = {}
+    // Object.defineProperty(Person, 'sex', {
+    //   configurable: true,
+    //   enumerable: true,
+    //   value: '男',
+    //   writable:false,
+    // })
+    // console.log('---------给person对象写入一个属性', Person);
+ 
+    // Person.sex = '女';
+    // console.log('----------修改后的person', Person);
   }
+  
 })
